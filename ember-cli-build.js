55ecf233a475f195f9bfb5e2d8a82b79eb151f18
James C. Davis <james@cos.io>
2020-01-02 00:48:45 -0500

    chore: disable hinting in ember build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
     const app = new EmberApp(defaults, {
-        // Add options here
+        hinting: false,
     });
 
     // Use `app.import` to add additional libraries to the generated
