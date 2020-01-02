252df56ca9dba4d0faf462cd142d4aca69b0c19a
James C. Davis <james@cos.io>
2020-01-02 00:46:12 -0500

    chore: fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,76 +1,73 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    ecmaFeatures: {
-      legacyDecorators: true
-    }
-  },
-  plugins: [
-    'ember'
-  ],
-  extends: [
-    'airbnb-base',
-    'eslint:recommended',
-    'plugin:ember/recommended'
-  ],
-  env: {
-    browser: true
-  },
-  rules: {
-    'ember/no-jquery': 'error',
-    'import/extensions': 'off',
-    'import/no-unresolved': 'off',
-    'import/no-extraneous-dependencies': 'off'
-  },
-  overrides: [
-    // node files
-    {
-      files: [
-        '.eslintrc.js',
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'lib/*/index.js',
-        'server/**/*.js'
-      ],
-      parserOptions: {
-        sourceType: 'script'
-      },
-      env: {
-        browser: false,
-        node: true
-      },
-      plugins: ['node'],
-      extends: [
-        'plugin:node/recommended'
-      ],
-      rules: {
-        // add your custom rules and overrides for node files here
-        strict: 'off',
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+            legacyDecorators: true,
+        },
+    },
+    plugins: ['ember'],
+    extends: ['airbnb-base', 'eslint:recommended', 'plugin:ember/recommended'],
+    env: {
+        browser: true,
+    },
+    rules: {
+        'ember/no-jquery': 'error',
+        'import/extensions': 'off',
+        'import/no-unresolved': 'off',
+        'import/no-extraneous-dependencies': 'off',
+        'func-names': 'off',
+    },
+    overrides: [
+        // node files
+        {
+            files: [
+                '.eslintrc.js',
+                '.template-lintrc.js',
+                'ember-cli-build.js',
+                'testem.js',
+                'blueprints/*/index.js',
+                'config/**/*.js',
+                'lib/*/index.js',
+                'server/**/*.js',
+            ],
+            parserOptions: {
+                sourceType: 'script',
+            },
+            env: {
+                browser: false,
+                node: true,
+            },
+            plugins: ['node'],
+            extends: ['plugin:node/recommended'],
+            rules: {
+                // add your custom rules and overrides for node files here
+                strict: 'off',
 
-        // this can be removed once the following is fixed
-        // https://github.com/mysticatea/eslint-plugin-node/issues/77
-        'node/no-unpublished-require': 'off'
-      },
-    },
-    {
-      files: ['*.js'],
-      extends: [
-        'plugin:prettier/recommended',
-      ],
-    },
-    {
-      files: ['*.ts'],
-      extends: [
-        'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint',
-        'plugin:prettier/recommended',
-      ],
-    }
-  ]
+                // this can be removed once the following is fixed
+                // https://github.com/mysticatea/eslint-plugin-node/issues/77
+                'node/no-unpublished-require': 'off',
+            },
+        },
+        {
+            files: ['app/router.js'],
+            rules: {
+                'array-callback-return': 'off',
+            },
+        },
+        {
+            files: ['*.js'],
+            extends: ['plugin:prettier/recommended'],
+        },
+        {
+            files: ['*.ts'],
+            extends: [
+                'plugin:@typescript-eslint/recommended',
+                'prettier/@typescript-eslint',
+                'plugin:prettier/recommended',
+            ],
+        },
+    ],
 };
