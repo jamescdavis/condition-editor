b77781ca929ee44240d24adb7b213fa71f6b3d7c
James C. Davis <james@cos.io>
2020-01-02 05:26:32 -0500

    feat: add types for window.datastore

--- types/condition-editor/index.d.ts
+++ types/condition-editor/index.d.ts
@@ -1,8 +1,43 @@
 import Ember from 'ember';
 
-declare global {
-  interface Array<T> extends Ember.ArrayPrototypeExtensions<T> {}
-  // interface Function extends Ember.FunctionPrototypeExtensions {}
+interface Product {
+    id: number;
+    property_values: PropertyValue[];
 }
 
-export {};
+interface PropertyValue {
+    property_id: number;
+    value: string | number;
+}
+
+interface Property {
+    id: number;
+    name: string;
+    type: PropertyType;
+}
+
+export enum PropertyType {
+    String = 'string',
+    Number = 'number',
+    Enumerated = 'enumerated',
+}
+
+export interface Operator {
+    text: string;
+    id: string;
+}
+
+interface Datastore {
+    getProducts(): Product[];
+    getProperties(): Property[];
+    getOperators(): Operator[];
+}
+
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface Array<T> extends Ember.ArrayPrototypeExtensions<T> {}
+    // interface Function extends Ember.FunctionPrototypeExtensions {}
+    interface Window {
+        datastore: Datastore;
+    }
+}
