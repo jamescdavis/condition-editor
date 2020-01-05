97a2ff6cba1fd7b169faf5ffab9c7db21e1e8f72
James C. Davis <james@cos.io>
2020-01-05 02:00:22 -0500

    feat: add operator map to property model

--- app/models/property.ts
+++ app/models/property.ts
@@ -1,10 +1,21 @@
 import Model, { attr } from '@ember-data/model';
+import { Id as Op } from 'condition-editor/lib/operator';
 
 export enum PropertyType {
     String = 'string',
     Number = 'number',
     Enumer = 'enumerated',
 }
+
+type OperatorMap = {
+    [propertyType in PropertyType]: Op[];
+};
+
+export const operatorMap: OperatorMap = {
+    [PropertyType.String]: [Op.Eq, Op.Any, Op.None, Op.In, Op.Contains],
+    [PropertyType.Number]: [Op.Eq, Op.Gt, Op.Lt, Op.Any, Op.None, Op.In],
+    [PropertyType.Enumer]: [Op.Eq, Op.Any, Op.None, Op.In],
+};
 
 export default class PropertyModel extends Model {
     @attr('string') name!: string;
