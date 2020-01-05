6fbf26b950dcbd22db12b969721e6427e9e0a99a
James C. Davis <james@cos.io>
2020-01-05 15:55:03 -0500

    tests: convert test-helper to ts

--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -1,7 +1,8 @@
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
-import Application from '../app';
+import Application from 'condition-editor/app';
 import config from '../config/environment';
+import 'qunit-dom';
 
 setApplication(Application.create(config.APP));
 
