e23e54b0f738eeb9e719972e3d7734837417ec3e
James C. Davis <james@cos.io>
2020-01-05 23:50:30 -0500

    feat: added basic input validations

---
+++ app/validations/input.ts
@@ -0,0 +1,38 @@
+import { ValidatorMapFunc } from 'ember-changeset/types';
+import { validateNumber, validateFormat } from 'ember-changeset-validations/validators';
+
+import { Id as Op } from 'condition-editor/lib/operator';
+import { PropertyType } from 'condition-editor/models/property';
+
+type OpValidationMap = {
+    [op in Op]: undefined | { input: ValidatorMapFunc };
+};
+
+type ValidationsMap = {
+    [propertyType in PropertyType]: {
+        default: undefined | { input: ValidatorMapFunc };
+    } & Partial<OpValidationMap>;
+};
+
+const validationsMap: Partial<ValidationsMap> = {
+    [PropertyType.String]: {
+        default: undefined,
+        [Op.In]: {
+            input: validateFormat({
+                regex: /^[^,]*[^\s,][^,]*(,[^,]*[^\s,][^,]*)*$/,
+                message: 'Must be a comma-separated list',
+            }),
+        },
+    },
+    [PropertyType.Number]: {
+        default: { input: validateNumber({ allowBlank: true, message: 'Must be a number' }) },
+        [Op.In]: {
+            input: validateFormat({
+                regex: /^\s*\d+(\.\d+)?\s*(,?\s*\d+(\.\d+)?\s*)*$/,
+                message: 'Must be a comma-separated list of numbers',
+            }),
+        },
+    },
+};
+
+export default validationsMap;
